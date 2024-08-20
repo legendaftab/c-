baf850ffa0af70052d5be2dc75923207b14ec538
legendaftab <aftabkazi533@gmail.com>
2024-08-20 11:05:26 +0530

    use basic command to run programe

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World! carry bahi" << std::endl;
+    std::cout << "Hello, World! carry bahi fdgb" << std::endl;
     return 0;
 }
+// imp to run file
+// g++ hello.cpp -o hello
+//  ./hello
